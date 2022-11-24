dd893dcf112d7983fe80523686887a7a47caad18
developer-mahin <mdmahin1310@gmail.com>
2022-11-24 18:57:29 +0600

    create a get method for get all the products

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -11,7 +12,6 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mfp1c6k.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -20,25 +20,40 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     const usersCollection = client.db("computer-zone").collection("users")
     const categoriesCollection = client.db("computer-zone").collection("categories")
-
+    const productsCollection = client.db("computer-zone").collection("products")
 
     try {
 
         // save user in the database
-        app.post("/users", async(req, res)=>{
+        app.post("/users", async (req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user)
             res.send(result)
         })
 
         // get all categories 
-        app.get("/categories", async(req,res)=>{
+        app.get("/categories", async (req, res) => {
             const query = {}
             const result = await categoriesCollection.find(query).toArray()
             res.send(result)
         })
 
-        
+        // get all products by category
+        app.get("/category/:id", async (req, res) => {
+            const id = req.params.id;
+            const se = parseInt(id)
+            const query = {category_id: se}
+            const products = await productsCollection.find(query).toArray()
+            res.send(products)
+        })
+
+        // get all products
+        app.get("/products", async(req,res)=>{
+            const query = {}
+            const result = await productsCollection.find(query).toArray()
+            res.send(result)
+        })
+
     }
     finally {
 
